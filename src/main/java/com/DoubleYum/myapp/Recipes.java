c3187f5019e481dc03786933eed34ee21b0d1efb
jayrogers <jannalrgrs@gmail.com>
2016-09-18 17:24:06 -0400

    added new variables and constructor for calories and cook time

--- src/main/java/com/DoubleYum/myapp/Recipes.java
+++ src/main/java/com/DoubleYum/myapp/Recipes.java
@@ -9,20 +9,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 public class Recipes {
 
 
-//add userinput to application
 	private String title = "";
 	private String image = "";
+	private int calories = "";
+	private int cookTime = "";
 	
-	
-	public Recipes(String title, String image) {
-		super();
+	public Recipes(String title, String image, calories, cookTime) {
+
 		this.image = image;
 		this.title = title;
+		this.calories;
+		this.cookTime;
 	}
+	
+	
+	public int getCalories() {
+		return calories;
+	}
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+	public int getCookTime() {
+		return cookTime;
+	}
+	public void setCookTime(int cookTime) {
+		this.cookTime = cookTime;
+	}
+	
 	public String getImage() {
 		return image;
 	}
@@ -35,7 +51,7 @@ public class Recipes {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 
 	
 }
